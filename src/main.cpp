66053b912d0734f712f2468cc3f2510934b8ce7b
felipepsaraiva <felipepiressaraiva@gmail.com>
2018-10-20 20:41:59 -0300

    Teste protocolo de comunicação

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,34 @@
 #include "mbed.h"
 #include "DataManager.h"
 
-// Serial pc(USBTX, USBRX, 115200);  // Usando minicom, o maximo é 115200
+Serial pc(USBTX, USBRX, 115200);  // Usando minicom, o maximo é 115200
 DigitalOut led1(LED1);
 
 void disable_all_enable_pins();
 
 int main() {
-  DataManager& data = DataManager::getInstance();
-  for (uint8_t i = 0; i < NUM_SERVOS; i++)
-    data.setRealPosition(i, i);
-
   led1 = true;
+  uint8_t header[4] = {0x8E, 0x02, 45, 0x48};
+  uint16_t data[20] = {
+    1, 2, 3, 4, 5, 6,
+    7, 8, 9, 10, 11, 12,
+    13, 14, 15, 16,  17, 18,
+    19, 20
+  };
 
-  Communication com(USBTX, USBRX, 115200);
-  com.start();
+  while (true) {
+    for (uint8_t i = 0; i < 4; i++)
+      pc.putc(header[i]);
+
+    for (uint8_t i = 0; i < 20; i++) {
+      pc.putc((data[i] >> 8) & 0xFF);
+      pc.putc(data[i] & 0xFF);
+    }
+
+    pc.putc(0xFF);
+
+    wait(1);
+  }
 }
 
 // int main() {
